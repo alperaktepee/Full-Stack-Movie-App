854624e6f891e472213a66b08cf5d32d159e6037
alperaktepee <alperaktepe98@gmail.com>
2023-02-15 09:24:56 +0300

    User saving to database

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,12 @@
-exports.createUser = (req, res) => {
-    console.log(req.body);
-  res.json({user:req.body})
+const User = require('../models/user')
+
+
+exports.createUser = async(req, res) => {
+const {name,email,password}=req.body;
+
+const newUser=new User({name,email,password});
+await newUser.save();
+  res.json({user:newUser})
 };
+
+
