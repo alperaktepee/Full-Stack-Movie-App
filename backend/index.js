acfd61464e416c2abdb8355816af20315dd5949a
alperaktepee <alperaktepe98@gmail.com>
2023-02-15 10:19:32 +0300

    Explanation of what is a middleware

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,23 @@ app.use(express.json());
 //Routes
 app.use("/api/user", userRouter);
 
+//I learned what is middleware 
+app.post(
+  "/sign-in",
+  (req, res, next) => {
+    const { email, password } = req.body;
+
+    //Check if email or password is empty
+    if (!email || !password) {
+      return res.json({ error: "E-mail or password cannot be empty !" });
+    }
+    next();
+  },
+  (req, res) => {
+    res.send("<h1>Hello I am about page</h1>");
+  }
+);
+
 //Server listening
 app.listen(8000, () => {
   console.log("The app is running on port 8000");
