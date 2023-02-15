d4cac1f53b798e5523c300046cc9bad2ecbf8d01
alperaktepee <alperaktepe98@gmail.com>
2023-02-15 09:41:40 +0300

    Hashing password with bcrypt

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,24 +1,37 @@
 //import mongoose
 const mongoose = require("mongoose");
+//import bcrypt
+const bcrypt = require("bcrypt");
 
 //creating user schema
 const userSchema = mongoose.Schema({
-    name:{
-        type:String,
-        trim:true,
-        required:true
-    },
-    email:{
-        type:String,
-        trim:true,
-        required:true,
-        unique:true
-    },
-    password:{
-        type:String,
-        required:true,
-    },
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+//hashing password
+userSchema.pre("save", async function (next) {
+  //It means: run this code before("PRE") "Save"
+
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10); //First is password, second is salt round
+  }
+
+  next();
 });
 
 //export model
-module.exports = mongoose.model("User",userSchema)
+module.exports = mongoose.model("User", userSchema);
