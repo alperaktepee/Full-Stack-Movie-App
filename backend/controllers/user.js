03e0097a40783a36c10ec0f4b935bf2690a058e5
alperaktepee <alperaktepe98@gmail.com>
2023-02-15 09:56:04 +0300

    Prevent duplicate emails and return status codes

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,12 +1,21 @@
-const User = require('../models/user')
+//import User model
+const User = require("../models/user");
 
+//createUser service
+exports.createUser = async (req, res) => {
+  //getting req from front
+  const { name, email, password } = req.body;
 
-exports.createUser = async(req, res) => {
-const {name,email,password}=req.body;
+  //look for email is already exists or not
+  const oldUser = await User.findOne({ email });
+  if (oldUser) {
+    return res.status(401).json({ error: "This e-mail is already exists !" });
+  }
+  //create newUser class
+  const newUser = new User({ name, email, password });
+  //save to database
+  await newUser.save();
 
-const newUser=new User({name,email,password});
-await newUser.save();
-  res.json({user:newUser})
+  //send back to user
+  res.status(201).json({ user: newUser });
 };
-
-
