d53156da29beb5fd676a32af085156f2b3f74eea
alperaktepee <alperaktepe98@gmail.com>
2023-02-15 11:42:02 +0300

    E-mail verification token model created

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -16,6 +16,15 @@ exports.create = async (req, res) => {
   //save to database
   await newUser.save();
 
+  // var transport = nodemailer.createTransport({
+  //   host: "sandbox.smtp.mailtrap.io",
+  //   port: 2525,
+  //   auth: {
+  //     user: "b0826494f7efde",
+  //     pass: "ea318af8a39d5f"
+  //   }
+  // });
+
   //send back to user
   res.status(201).json({ user: newUser });
 };
